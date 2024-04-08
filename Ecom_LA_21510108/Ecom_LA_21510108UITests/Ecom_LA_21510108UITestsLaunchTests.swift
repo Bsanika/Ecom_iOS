8fc8708049f50b40708763519f70cf0ef2a2a931
Bsanika <162365214+Bsanika@users.noreply.github.com>
2024-04-08 17:39:20 +0530

    Add files via upload

---
+++ Ecom_LA_21510108/Ecom_LA_21510108UITests/Ecom_LA_21510108UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Ecom_LA_21510108UITestsLaunchTests.swift
+//  Ecom_LA_21510108UITests
+//
+//  Created by Apple Lab 10 on 08/04/24.
+//
+
+import XCTest
+
+final class Ecom_LA_21510108UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
